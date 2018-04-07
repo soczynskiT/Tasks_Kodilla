c8d393f2d29fa047635fb13c2f5c0436a8f0f91d
“soczynskiT <t.soczynski86@gmail.com>
2018-04-07 10:44:29 +0200

    Module 14.3.1

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,39 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+
+    @GetMapping(value = "getTasks")
+    public List<Task> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "getTask")
+    public TaskDto getTask(String taskId) {
+        return new TaskDto((long) 1, "test_title", "test_content");
+    }
+
+    @DeleteMapping(value = "deleteTask")
+    public void deleteTask(String taskId) {
+
+    }
+
+    @PutMapping(value = "updateTask")
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto((long) 1, "edited_title", "test_content");
+
+    }
+
+    @PostMapping(value = "createTask")
+    public void createTask(TaskDto taskDto) {
+
+    }
+}
